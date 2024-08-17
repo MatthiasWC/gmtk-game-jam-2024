6029b8bc51abeef35673b3d1d274b602db8f91d9
Matthias Claassen <matthiasclaassen01@gmail.com>
2024-08-17 18:01:24 -0400

    Got block controller working

--- Assets/Scripts/GameBounds.cs
+++ Assets/Scripts/GameBounds.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class GameBounds : MonoBehaviour
 {
     public static GameBounds instance;
-    
+
+    public Vector2 lowerLeft;
+    public Vector2 lowerRight;
+    public Vector2 upperRight;
+    public Vector2 upperLeft;
+
     void Start()
     {
         if (instance != null && instance != this)
@@ -16,5 +21,12 @@ public class GameBounds : MonoBehaviour
         {
             instance = this;
         }
+
+        Vector2[] points = GetComponent<EdgeCollider2D>().points;
+
+        lowerLeft = points[0];
+        lowerRight = points[1];
+        upperLeft = points[2];
+        upperRight = points[3];
     }
 }
