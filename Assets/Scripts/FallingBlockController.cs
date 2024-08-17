e02c2fafcde1bbd26fcb76378a5b4c18d4c36731
Matthias Claassen <matthiasclaassen01@gmail.com>
2024-08-17 12:49:08 -0400

    Added scripts folder & began on falling block controller

---
+++ Assets/Scripts/FallingBlockController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlockController : MonoBehaviour
+{
+    [SerializeField]
+    private float fallSpeed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -fallSpeed);
+    }
+}
