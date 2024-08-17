ebe326ebf5b9f48b5a3855583c9d30cf96cd7720
sepha91 <leerosun14@gmail.com>
2024-08-17 15:33:32 -0500

    finished player movement prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpSpeed = 15f;
+    public float initialJumpSpeed = 5;
+    // public float jumpAcceleration = 0f;
+    public float maxJumpTime = 0.1f;
+    public float earlyJumpReleaseVelocityModifier = 0.1f;
 
     private Rigidbody2D rb;
     private BoxCollider2D col;
-    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        col = rb.GetComponent<BoxCollider2D>();
+        col = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -24,19 +27,44 @@ public class PlayerMovement : MonoBehaviour
 
         // move left/right
         newVelocity.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        if (HoldingJumpKey()) newVelocity.y = 1;
 
         // jump
-        if (HoldingJumpKey() && IsGrounded())
+        if (PressedJumpKey() && IsGrounded())
         {
-            newVelocity.y = jumpSpeed;
-        }
-        if (!HoldingJumpKey() && rb.velocity.y > 0)
-        {
-            newVelocity.y = 0;
+            newVelocity.y = initialJumpSpeed;
+            StartCoroutine(Jump());
         }
 
         rb.velocity = newVelocity;
+    }
+
+    IEnumerator Jump()
+    {
+        Debug.Log("Jumping");
+        float currentJumpTime = 0f;
+
+        while (currentJumpTime < maxJumpTime && HoldingJumpKey())
+        {
+            currentJumpTime += Time.deltaTime;
+            Debug.Log("in jump coroutine");
+            yield return null;
+        }
+
+        if (!HoldingJumpKey() && rb.velocity.y > 0)
+        {
+            Debug.Log("kill velocity early");
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * earlyJumpReleaseVelocityModifier);
+            yield break;
+        }
+    }
+
+
+    bool PressedJumpKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) return true;
+        if (Input.GetKeyDown(KeyCode.W)) return true;
+        return false;
+
     }
 
     bool HoldingJumpKey()
@@ -47,17 +75,23 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    bool ReleasedJumpKey()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) return true;
+        if (Input.GetKeyUp(KeyCode.W)) return true;
+        return false;
+    }
+
     bool IsGrounded()
     {
-        Vector2 origin = col.bounds.center;
-
-        float smallRadius = col.bounds.size.y - 0.05f;
-
-        float bigRadius = col.bounds.size.y + 0.05f;
-
-        if (Physics2D.CircleCast(origin, smallRadius, Vector2.down, bigRadius))
+        Vector2 origin = transform.position;
+        origin.y = origin.y - col.bounds.extents.y - 0.05f;
+        if (Physics2D.OverlapBox(origin,new Vector2(col.bounds.size.x-0.1f,0.05f),0))
+        {
+            // Debug.Log("grounded");
             return true;
+        }
+        // Debug.Log("not grounded");
         return false;
-
     }
 }
