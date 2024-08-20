e73abef4af84ecadfc5e717d081962f86a6cefd2
Matthias Claassen <matthiasclaassen01@gmail.com>
2024-08-19 22:20:27 -0400

    Added win & death menus

--- Assets/Scripts/damage.cs
+++ Assets/Scripts/damage.cs
@@ -5,23 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class damage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log("collided");
+            DeathMenu.instance.Open();
+            Time.timeScale = 0;
         }
     }
 }
